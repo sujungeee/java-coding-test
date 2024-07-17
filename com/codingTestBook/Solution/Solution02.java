010e74a26ed6685e858fed0ed781e5d5b56a4124
sujung0106 <sujung0106e.e@gmail.com>
2024-07-17 22:10:27 +0900

    update 코테합격자되기/문제 1, 2

--- com/codingTestBook/Solution/Solution02.java
+++ com/codingTestBook/Solution/Solution02.java
@@ -5,14 +5,14 @@ import java.util.Collections;
 import java.util.TreeSet;
 
 public class Solution02 {
-    public static int[] solution02(int[] arr){ // by 스트림
+    public static int[] solution02_1(int[] arr){ // by 스트림
         Integer[] result= Arrays.stream(arr).boxed().distinct().toArray(Integer[]::new);
         Arrays.sort(result, Collections.reverseOrder());
 
         return Arrays.stream(result).mapToInt(Integer::intValue).toArray();
     }
 
-    public static int[] solution02_1(int[] arr){ // by TreeSet
+    public static int[] solution02_2(int[] arr){ // by TreeSet
         TreeSet<Integer> set= new TreeSet<>(Collections.reverseOrder());
         for(int num: arr){
             set.add(num);
@@ -27,7 +27,7 @@ public class Solution02 {
     }
 
     public static void main(String[] args){
-        System.out.println(Arrays.toString(solution02(new int[]{4, 2, 2, 1, 3, 4})));
-        System.out.println(Arrays.toString(solution02_1(new int[]{2, 1, 1, 3, 2, 5, 4})));
+        System.out.println(Arrays.toString(solution02_1(new int[]{4, 2, 2, 1, 3, 4})));
+        System.out.println(Arrays.toString(solution02_2(new int[]{2, 1, 1, 3, 2, 5, 4})));
     }
 }
