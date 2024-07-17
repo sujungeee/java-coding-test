e75de01d6f473dbbab7b699cf773c4ab87015363
sujung0106 <sujung0106e.e@gmail.com>
2024-07-17 22:08:44 +0900

    add 코테합격자되기/문제 1, 2

---
+++ com/codingTestBook/Solution/Solution01.java
@@ -0,0 +1,22 @@
+package com.codingTestBook.Solution;
+
+import java.util.Arrays;
+
+public class Solution01 {
+    public static int[] solution01(int[] arr) { // 원본을 바꾸는 정렬
+        Arrays.sort(arr);
+        return arr;
+    }
+
+    public static int[] solution01_1(int[] arr){ // 원본 배열을 복사한 새 배열을 정렬
+        int[] clone= arr.clone(); // 파이썬의 .copy()와 같은 기능
+        Arrays.sort(clone);
+        return clone;
+    }
+
+    public static void main(String[] args){
+        System.out.println(Arrays.toString(solution01(new int[]{1, -5, 2, 4, 3})));
+        System.out.println(Arrays.toString(solution01(new int[]{2, 1, 1, 3, 2, 5, 4})));
+        System.out.println(Arrays.toString(solution01(new int[]{6, 1, 7})));
+    }
+}
---
+++ com/codingTestBook/Solution/Solution02.java
@@ -0,0 +1,33 @@
+package com.codingTestBook.Solution;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.TreeSet;
+
+public class Solution02 {
+    public static int[] solution02(int[] arr){ // by 스트림
+        Integer[] result= Arrays.stream(arr).boxed().distinct().toArray(Integer[]::new);
+        Arrays.sort(result, Collections.reverseOrder());
+
+        return Arrays.stream(result).mapToInt(Integer::intValue).toArray();
+    }
+
+    public static int[] solution02_1(int[] arr){ // by TreeSet
+        TreeSet<Integer> set= new TreeSet<>(Collections.reverseOrder());
+        for(int num: arr){
+            set.add(num);
+        }
+        // int 형 배열에 담아서 반환
+        int[] result= new int[set.size()];
+        for(int i=0; i<result.length; i++){
+            result[i]= set.pollFirst();
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args){
+        System.out.println(Arrays.toString(solution02(new int[]{4, 2, 2, 1, 3, 4})));
+        System.out.println(Arrays.toString(solution02_1(new int[]{2, 1, 1, 3, 2, 5, 4})));
+    }
+}
