0aecb29874283b02fd5f39098352f5e7edce9661
sujung0106 <sujung0106e.e@gmail.com>
2024-08-06 21:32:31 +0900

    add C155652.java(프로그래머스)

---
+++ com/programmers/C155652.java
@@ -0,0 +1,31 @@
+package com.programmers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class C155652 {
+    public static String solution155652(String s, String skip, int index){
+        StringBuilder sb= new StringBuilder();
+        List<Character> list= new ArrayList<>();
+        for(int i=0; i<26; i++){
+            list.add((char)('a'+i));
+        }
+
+        // skip은 제거
+        for(Character item: skip.toCharArray()){
+            list.remove(item);
+        }
+
+        for(Character ch: s.toCharArray()){
+            int idx= list.indexOf(ch);
+            idx= (idx+index) % list.size();
+            sb.append(list.get(idx));
+        }
+
+        return sb.toString();
+    }
+
+    public static void main(String args[]){
+        System.out.println(solution155652("aukks", "wbqd", 5));
+    }
+}
