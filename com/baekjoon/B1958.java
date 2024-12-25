4783ecb119949e62d98c3fb4e8f27eb0ecf832c6
남수정 <2020113388@dgu.ac.kr>
2024-12-25 18:31:47 +0900

    [241225] add B1958.java(LCS 3)

---
+++ com/baekjoon/B1958.java
@@ -0,0 +1,45 @@
+// https://www.acmicpc.net/problem/1958
+// 1958: LCS 3
+package com.baekjoon;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+
+public class B1958 {
+    public static String str1;
+    public static String str2;
+    public static String str3;
+    public static int[][][] dp;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+        str1= br.readLine();
+        str2= br.readLine();
+        str3= br.readLine();
+
+        System.out.println(LCS(str1, str2, str3));
+    }
+
+    public static int LCS(String s1, String s2, String s3) {
+        StringBuilder sb= new StringBuilder();
+        int n= s1.length();
+        int m= s2.length();
+        int l= s3.length();
+
+        dp= new int[n+1][m+1][l+1];
+
+        for (int i=1; i<n+1; i++) {
+            for (int j=1; j<m+1; j++) {
+                for (int k=1; k<l+1; k++) {
+                    if (s1.charAt(i-1) == s2.charAt(j-1) && s2.charAt(j-1) == s3.charAt(k-1)) {
+                        dp[i][j][k]= dp[i-1][j-1][k-1] + 1;
+                    } else {
+                        dp[i][j][k]= Math.max(dp[i-1][j][k], Math.max(dp[i][j-1][k], dp[i][j][k-1]));
+                    }
+                }
+            }
+        }
+        return dp[n][m][l];
+    }
+}
