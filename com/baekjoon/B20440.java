723f61f04a59eaad3c1213ca9b9755f1f09e9920
남수정 <2020113388@dgu.ac.kr>
2025-01-07 17:39:38 +0900

    [250107] add B2044.java(🎵니가 싫어 싫어 너무 싫어 싫어 오지 마 내게 찝쩍대지마🎵 - 1)

--- com/baekjoon/B20440.java
+++ com/baekjoon/B20440.java
@@ -6,7 +6,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.BufferedReader;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.StringTokenizer;
 
