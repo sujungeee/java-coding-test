b12d55db2cc51db8a938b1f38bb9bf4abeb4737f
남수정 <2020113388@dgu.ac.kr>
2025-01-18 15:10:29 +0900

    [250116] add B2668.java(숫자고르기)

--- com/baekjoon/B2668.java
+++ com/baekjoon/B2668.java
@@ -7,72 +7,48 @@ import java.io.InputStreamReader;
 import java.io.BufferedReader;
 import java.util.*;
 
-import static java.lang.System.exit;
-
 public class B2668 {
+    public static List<Integer> list;
     public static int N;
-    public static int[][] maps;
-    public static int[] idxList;
+    public static int[] arr;
     public static boolean[] visited;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
 
+        list= new ArrayList<>();
+
         N= Integer.parseInt(br.readLine());
+        visited= new boolean[N+1];
 
-        idxList= new int[N];
-        visited= new boolean[N];
-
-        maps= new int[2][N];
-        for(int i=0; i<N; i++) {
-            maps[0][i]= i+1;
-            maps[1][i]= Integer.parseInt(br.readLine());
-            idxList[i]= i;
+        arr= new int[N+1];
+        for(int i=1; i<N+1; i++) {
+            arr[i]= Integer.parseInt(br.readLine());
         }
 
-        for(int i=N; i>=0; i--){
-            dfs(visited, 0, i, i); // 집합의 개수 => N combinations r
-        }
-    }
-
-    public static void dfs(boolean[] visited, int start, int r, int cnt) {
-        if (r == 0) {
-            if (is_same()) {
-                System.out.println(cnt-1);
-                print();
-                exit(0);
-            }
-            return;
-        } else {
-            for(int i= start; i < N; i++) {
+        for(int i=1; i<N+1; i++) {
+            if (!visited[i]) {
                 visited[i]= true;
-                dfs(visited, i + 1, r - 1, i);
+                dfs(i, i);
                 visited[i]= false;
             }
         }
+
+        System.out.println(list.size());
+        for(int i: list) {
+            System.out.println(i);
+        }
     }
 
-    // 두 집합이 같은지 확인하는 함수
-    public static boolean is_same() {
-        Set<Integer> set1= new HashSet<>();
-        Set<Integer> set2= new HashSet<>();
-
-        for (int i=0; i<N; i++) {
-            if (visited[i]) {
-                set1.add(maps[0][i]);
-                set2.add(maps[1][i]);
-            }
+    public static void dfs(int start, int idx) {
+        if (arr[idx] == start) {
+            list.add(start);
         }
 
-        return set1.equals(set2);
-    }
-
-    // 원소 구성이 같은 두 집합의 인덱스들을 출력하는 함수
-    public static void print() {
-        for (int i=0; i<N; i++) {
-            if (visited[i]) {
-                System.out.println(i+1);
-            }
+        if (!visited[arr[idx]]) {
+            visited[arr[idx]] = true;
+            dfs(start, arr[idx]);
+            visited[arr[idx]] = false;
         }
     }
 }
